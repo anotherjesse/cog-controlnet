7b893ec151da5fcffc7f8dcb7dda8f04ba4ef7e1
Dan <dan.nelson8@gmail.com>
2023-06-14 22:35:38 +0000

    uncommenting testing

--- samples.py
+++ samples.py
@@ -29,62 +29,62 @@ def gen(output_fn, **kwargs):
 
 
 def main():
-    # gen(
-    #     "sample.depth.png",
-    #     structure="depth",
-    #     prompt="taylor swift, best quality, extremely detailed",
-    #     image="https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png",
-    #     seed=42,
-    #     steps=30,
-    # )
-    # gen(
-    #     "sample.canny.png",
-    #     prompt="taylor swift, best quality, extremely detailed",
-    #     structure="canny",
-    #     image="https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png",
-    #     seed=42,
-    #     steps=20,
-    # )
-    # gen(
-    #     "sample.normal.png",
-    #     structure="normal",
-    #     prompt="taylor swift, best quality, extremely detailed",
-    #     image="https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png",
-    #     seed=42,
-    #     steps=30,
-    # )
-    # gen(
-    #     "sample.seg.png",
-    #     structure="seg",
-    #     prompt="mid century modern bedroom",
-    #     image="https://huggingface.co/takuma104/controlnet_dev/resolve/main/gen_compare/control_images/room_512x512.png",
-    #     seed=42,
-    #     steps=30,
-    # )
-    # gen(
-    #     "sample.hed.png",
-    #     structure="hed",
-    #     image="https://huggingface.co/takuma104/controlnet_dev/resolve/main/gen_compare/control_images/bird_512x512.png",
-    #     prompt="rainbow bird, best quality, extremely detailed",
-    #     seed=42,
-    #     steps=30,
-    # )
-    # gen(
-    #     "sample.pose.png",
-    #     structure="pose",
-    #     image="https://hf.co/datasets/YiYiXu/controlnet-testing/resolve/main/yoga1.jpeg",
-    #     prompt="farmer yoga pose, best quality, extremely detailed",
-    #     seed=42,
-    #     steps=30,
-    # )
-    # gen(
-    #     "sample.hough.png",
-    #     structure="hough",
-    #     prompt="mid century modern bedroom",
-    #     image="https://huggingface.co/takuma104/controlnet_dev/resolve/main/gen_compare/control_images/room_512x512.png",
-    #     seed=42,
-    #     steps=30,
-    # )
+    gen(
+        "sample.depth.png",
+        structure="depth",
+        prompt="taylor swift, best quality, extremely detailed",
+        image="https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png",
+        seed=42,
+        steps=30,
+    )
+    gen(
+        "sample.canny.png",
+        prompt="taylor swift, best quality, extremely detailed",
+        structure="canny",
+        image="https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png",
+        seed=42,
+        steps=20,
+    )
+    gen(
+        "sample.normal.png",
+        structure="normal",
+        prompt="taylor swift, best quality, extremely detailed",
+        image="https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png",
+        seed=42,
+        steps=30,
+    )
+    gen(
+        "sample.seg.png",
+        structure="seg",
+        prompt="mid century modern bedroom",
+        image="https://huggingface.co/takuma104/controlnet_dev/resolve/main/gen_compare/control_images/room_512x512.png",
+        seed=42,
+        steps=30,
+    )
+    gen(
+        "sample.hed.png",
+        structure="hed",
+        image="https://huggingface.co/takuma104/controlnet_dev/resolve/main/gen_compare/control_images/bird_512x512.png",
+        prompt="rainbow bird, best quality, extremely detailed",
+        seed=42,
+        steps=30,
+    )
+    gen(
+        "sample.pose.png",
+        structure="pose",
+        image="https://hf.co/datasets/YiYiXu/controlnet-testing/resolve/main/yoga1.jpeg",
+        prompt="farmer yoga pose, best quality, extremely detailed",
+        seed=42,
+        steps=30,
+    )
+    gen(
+        "sample.hough.png",
+        structure="hough",
+        prompt="mid century modern bedroom",
+        image="https://huggingface.co/takuma104/controlnet_dev/resolve/main/gen_compare/control_images/room_512x512.png",
+        seed=42,
+        steps=30,
+    )
     gen(
         "sample.scribble.png",
         structure="scribble",
